c5ac3304c02930acbdf8b5d4cd05eb14b37ebd1e
mohamed00012 <52926834+mohamed00012@users.noreply.github.com>
2022-11-11 14:20:50 +0200

    Add files via upload

---
+++ BUTTON.c
@@ -0,0 +1,6 @@
+/*
+ * BUTTON.c
+ *
+ * Created: 11/8/2022 3:45:05 PM
+ *  Author: Technology
+ */ 
---
+++ BUTTON.h
@@ -0,0 +1,16 @@
+/*
+ * BUTTON.h
+ *
+ * Created: 11/8/2022 3:44:49 PM
+ *  Author: Technology
+ */ 
+
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+
+void led_on(port,ledpin)
+
+
+#endif /* BUTTON_H_ */
